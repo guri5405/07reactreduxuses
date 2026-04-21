0a820cf9e7ef86b9b4d869508ea7eb42e7b811fb
Guree sandhu <113997269+guri5405@users.noreply.github.com>
2026-04-21 13:49:04 +0530

    Initialize App component with AddTodo and Todos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
+
+function App() {
+  return (
+    <div>
+      <h1>Learn about react redux</h1>
+      <AddTodo />
+      <Todos />
+    </div>
+  )
+}
+
+export default App
